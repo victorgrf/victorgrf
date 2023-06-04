3cf5c0caa0048b9e292eb1be4870eee8df8150d6
Victor G. Ramos <69207529+Ka-Konata@users.noreply.github.com>
2023-06-04 07:54:18 -0300

    teste

--- static/scripts/info-btns.js
+++ static/scripts/info-btns.js
@@ -5,18 +5,28 @@ function movePage(reverse) {
     if(reverse == true) {
         if(actualPage > 0) {
             actualPage -= 1;
-            changePageDetails()
+            changePageDetails();
         }
     } 
     
     else {
         if(actualPage < totalPages) {
             actualPage += 1;
-            changePageDetails()
+            changePageDetails();
         }
     }
 }
 
 function changePageDetails() {
-    console.log(actualPage)
+    console.log(actualPage);
+}
+
+function showMenu() {
+    let menu = $("#menu");
+    if(menu.hasClass("occulted")) {
+        menu.removeClass("occulted");
+    }
+    else {
+        menu.addClass("occulted");
+    }
 }
